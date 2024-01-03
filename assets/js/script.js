dbc4dfd5e3ed9efde37a57ae5c2e467b56552b2e
RobertoBerrio <robertocarlosberrio@gmail.com>
2024-01-02 19:12:07 -0500

    Portafolio web

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,3 +68,70 @@ const languageList = document.querySelector('.logo-container__language ul');
 languageIcon.addEventListener('click', ()=> {
     languageList.classList.toggle('click-ul');
 });
+
+//Animacion scroll
+const aboutContent = document.querySelector('.about-content');
+const projectsTitle = document.querySelector('.projects-title h2');
+const cardProject = document.querySelectorAll('.card-project');
+const contactTitle = document.querySelector('.contact-title h2');
+const contactContent = document.querySelector('.contact-content');
+const imgProfile = document.querySelector('.container-image img');
+
+window.addEventListener('scroll', ()=> {
+    const scroll = window.scrollY;
+    console.log(scroll);
+    
+    //Scroll contenido sobre mi
+    if (scroll >= 500) {
+        aboutContent.style.opacity = 1;
+        aboutContent.style.top = 0;
+
+        imgProfile.style.transform = 'scale(1)';
+        imgProfile.style.top = '-60px';
+        imgProfile.style.opacity = 1;
+    } else {
+        aboutContent.style.opacity = 0;
+        aboutContent.style.top = '200px';
+
+        imgProfile.style.transform = 'scale(0)';
+        imgProfile.style.top = 0;
+        imgProfile.style.opacity = 0;
+    }
+
+    //Scroll titulo proyectos
+    if (scroll >= 1200) {
+        projectsTitle.style.top = 0;
+        projectsTitle.style.opacity = 1;
+    } else {
+        projectsTitle.style.top = '100px';
+        projectsTitle.style.opacity = 0;
+    }
+
+    //Scroll contenido proyectos
+    if (scroll >= 1400) {
+        cardProject.forEach(e => {
+            e.style.top = 0;
+            e.style.opacity = 1;
+        })
+    } else {
+        cardProject.forEach(e => {
+            e.style.top = '150px';
+            e.style.opacity = 0;
+        })
+    }
+
+    //Scroll contenido contacto
+    if (scroll >= 2000) {
+        contactTitle.style.top = 0;
+        contactTitle.style.opacity = 1;
+
+        contactContent.style.top = 0;
+        contactContent.style.opacity = 1;
+    } else {
+        contactTitle.style.top = '150px';
+        contactTitle.style.opacity = 0;
+
+        contactContent.style.top = '150px';
+        contactContent.style.opacity = 0;
+    }
+});
